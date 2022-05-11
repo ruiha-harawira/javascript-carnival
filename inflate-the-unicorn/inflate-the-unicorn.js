e3037f6750700e9424373a7d8d1ee1b6b60d77e6
ruiha-harawira <ruiha.harawira@hotmail.com>
2022-05-11 14:52:59 +1200

    inflate unicorn

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,57 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Inflate The Unicorn!")
+console.log('Inflate The Unicorn!')
+
+let unicorns = document.getElementsByClassName('inflate-an-image')
+
+for (let i = 0; i < unicorns.length; i++) {
+  unicorns[i].onclick = unicornClick
+}
+
+let balloonInflate = 0
+
+function unicornClick(e) {
+  let unicorn = e.target
+
+  if (unicorn.id == 'unicorn-1') {
+    balloonInflate++
+
+    unicorn.src = './images/unicorn-' + balloonInflate + '.png'
+    console.log(unicorn)
+
+    if (balloonInflate > 3) {
+      balloonInflate = 0
+      unicorn.src = './images/unicorn-' + balloonInflate + '.png'
+      console.log('Thanks')
+      alert('My horn is complete, Thank you!')
+    }
+  }
+  if (unicorn.id == 'unicorn-2') {
+    balloonInflate++
+
+    unicorn.src = './images/unicorn-' + balloonInflate + '.png'
+    console.log(unicorn)
+
+    if (balloonInflate > 3) {
+      balloonInflate = 0
+      unicorn.src = './images/unicorn-' + balloonInflate + '.png'
+      console.log('Thanks')
+      alert('My horn is complete, Thank you!')
+    }
+  }
+  if (unicorn.id == 'unicorn-3') {
+    balloonInflate++
+
+    unicorn.src = './images/unicorn-' + balloonInflate + '.png'
+    console.log(unicorn)
+
+    if (balloonInflate > 3) {
+      balloonInflate = 0
+      unicorn.src = './images/unicorn-' + balloonInflate + '.png'
+      console.log('Thanks')
+      alert('My horn is complete, Thank you!')
+    }
+  }
+}
